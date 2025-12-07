336ea2b58a6665975282add8d2f2e800cdd54bbc
Tommaso <tommaso.g@pazzo.com>
2025-12-07 19:48:26 +0100

    Cazz

--- tests/tests_my_functions.py
+++ tests/tests_my_functions.py
@@ -4,4 +4,4 @@ def test_haversine():
     # Amsterdam to Berlin
     assert myfunctions.haversine(
         4.895168, 52.370216, 13.404954, 52.520008
-    ) == 576.6625818456291
+    ) == 570.6625818456291
